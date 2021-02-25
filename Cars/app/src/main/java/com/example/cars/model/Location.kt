f67143c12314b5eee5d7c1a999fd5447f8fe2e62
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-02-25 23:12:55 +0300

    new

--- Cars/app/src/main/java/com/example/cars/model/Location.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cars.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Location (
-    @SerializedName("cityName") val cityName : String,
-    @SerializedName("townName") val townName : String
-)
