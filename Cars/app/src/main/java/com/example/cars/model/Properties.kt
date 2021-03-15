ec3d38dbfb055b7d1e30be5e7268245b4c501f5c
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-15 20:27:48 +0300

    a

--- Cars/app/src/main/java/com/example/cars/model/Properties.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cars.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Properties (
-    @SerializedName("name") val name : String,
-    @SerializedName("value") val value :  String
-)
--- Cars/app/src/main/java/com/example/cars/util/CarClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cars.util
-
-import android.view.View
-
-interface CarClickListener {
-
-
-    fun carClick(view: View)
-}
